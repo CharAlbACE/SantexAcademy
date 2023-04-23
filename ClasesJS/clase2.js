c543f261907eeabd0e8295657e19fd24998fd570
charly <albornozcarlos04@gmail.com>
2023-04-23 01:37:47 -0300

    Tarea XAcademy

---
+++ ClasesJS/clase2.js
@@ -0,0 +1,20 @@
+// EXPLICACION DE FUNTION:
+
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+}
+
+saludar("Gaston");
+saludar("Pedro");
+saludar("Axel");
+saludar("Diego");
+saludar("Leonardo");
+
+
+
+function suma (n1,n2) {
+    return n1 + n2;
+}
+
+const resultado = suma(1, 4);
+console.log("resultado suma = " + resultado)
